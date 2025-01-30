77c0b2a2c13213482a4f8a69c330c2e17661a2fb
Craig Sykes <craig.sykes@musgrave.ie>
2025-01-30 23:28:38 +0000

    gallery using lightgallery.js, no images uploaded yet

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,5 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-export default defineConfig({
-  site: 'https://CraigSykes119.github.io/Davmar_House_Project/', // Your GitHub Pages URL
-  base: '/Davmar_House_Project/', // Your repository name
-  output: 'static', // Ensure Astro builds a static site
-});
+export default defineConfig({});
+
+
